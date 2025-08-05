dd6977e48cf6ba7ecc818606cc715000874b2547
Ankush Sharma <166238753+010Ankushsharma@users.noreply.github.com>
2025-08-05 21:08:00 +0530

    Add files via upload

---
+++ Connect/src/test/java/org/connect/ConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
